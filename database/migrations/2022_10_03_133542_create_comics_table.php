9c331ee441acfaad2de6d4cc4aff247493539ab2
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-10-03 15:49:55 +0200

    added $table entity for migration

--- database/migrations/2022_10_03_133542_create_comics_table.php
+++ database/migrations/2022_10_03_133542_create_comics_table.php
@@ -15,6 +15,12 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description')->nullable();
+            $table->string('price', 10);
+            $table->string('series', 50);
+            $table->string('sale_date', 10);
+            $table->string('type', 20);
             $table->timestamps();
         });
     }
