15fc18a5a930b7e927258234fef1cd71dcd73760
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-10-04 17:49:38 +0200

    add: added a form in create view

---
+++ resources/views/comic/create.blade.php
@@ -0,0 +1,48 @@
+@extends ('layout.app')
+@section ('title', 'Create your Comic')
+
+@section ('content')
+
+<div class="container">
+    <form action="{{route('comics.store')}}" method="POST">
+
+        @csrf
+        
+            <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" name="title" >
+            </div>
+    
+            <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <input type="text" class="form-control" id="description" name="description">
+            </div>
+    
+            <div class="mb-3">
+            <label for="price" class="form-label">Price</label>
+            <input type="text" class="form-control" id="price" name="price">
+            </div>
+    
+            <div class="mb-3">
+                <label for="series" class="form-label">Series</label>
+                <input type="text" class="form-control" id="series" name="series">
+                </div>
+    
+            <div class="mb-3">
+                <label for="sale_date" class="form-label">Sale Date</label>
+                <input type="text" class="form-control" id="sale_date" name="sale_date">
+            </div>
+    
+            <label for="type" class="form-label">Type</label>
+            <select name="type" id="type" class="form-select" aria-label="Default select example">
+                <option value="1">comic book</option>
+                <option value="2">graphic novel</option>
+              </select>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    
+</div>
+
+
+@endsection 
